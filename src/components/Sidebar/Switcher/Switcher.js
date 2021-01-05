02b63f6459ad3146c5448ac885230386df5d5d56
Ronit Mathur <38735296+Ronit-Mathur@users.noreply.github.com>
2021-01-05 14:54:13 -0800

    window fix, removed darkmode switch

--- src/components/Sidebar/Switcher/Switcher.js
+++ src/components/Sidebar/Switcher/Switcher.js
@@ -1,26 +1,27 @@
 import React, {useState} from 'react';
 import styles from './Switcher.module.scss';
+import window from 'global';
+import root from 'window-or-global';
 
 const Switcher = () => {
-  const [isDarkModeOn, setIsDarkModeOn] = useState(
-    !!(window.localStorage.getItem('darkMode')
-      || window.matchMedia('(prefers-color-scheme: dark)').matches)
-  );
-  const bodyEl = document.getElementsByTagName('body')[0];
+  // const [isDarkModeOn, setIsDarkModeOn] = useState(
+  //   !!(root.localStorage.getItem('darkMode')
+  //     || root.matchMedia('(prefers-color-scheme: dark)').matches)
+  // );
+  // const bodyEl = document.getElementsByTagName('body')[0];
 
-  function turnDarkMode() {
-    setIsDarkModeOn(!isDarkModeOn);
-    isDarkModeOn ? bodyEl.classList.add('dark') : bodyEl.classList.remove('dark');
-    window.localStorage.setItem('darkMode', isDarkModeOn);
-  }
+  // function turnDarkMode() {
+  //   setIsDarkModeOn(!isDarkModeOn);
+  //   isDarkModeOn ? bodyEl.classList.add('dark') : bodyEl.classList.remove('dark');
+  //   root.localStorage.setItem('darkMode', isDarkModeOn);
+  // }
+  //<input id='DarkModeSwitcher' type='checkbox' defaultChecked={!isDarkModeOn} />
+  //<span className={`${styles['slider']}`} onClick={() => turnDarkMode()}/>
 
   return (
     <div className={styles['switch__container']}>
       <span className={styles['switch__text']}>Night mode</span>
-      <label className={styles['switch']}>
-        <input id='DarkModeSwitcher' type='checkbox' defaultChecked={!isDarkModeOn} />
-        <span className={`${styles['slider']}`} onClick={() => turnDarkMode()}/>
-      </label>
+      <label className={styles['switch']}></label>
     </div>
   );
 };
