ce5f3a5e4fb52ad0b2e3070a34f7674d25bfcce7
Ronit Mathur <38735296+Ronit-Mathur@users.noreply.github.com>
2021-02-14 01:53:37 -0800

    Custom Pages

--- config.js
+++ config.js
@@ -1,55 +1,55 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://ronit-mathur.netlify.app/',
-  pathPrefix: '/',
-  title: 'Ronits Thought Bucket',
-  subtitle: 'Development & Psychology With a Hint of Introspection',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://ronit-mathur.netlify.app/",
+  pathPrefix: "/",
+  title: "Ronits Thought Bucket",
+  subtitle: "Development & Psychology With a Hint of Introspection",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   useKatex: false,
   darkFunctionalityIsOn: true,
   menu: [
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about",
     },
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
+      label: "Contact me",
+      path: "/pages/contacts",
+    },
     {
-      label: 'Projects',
-      path: '/Projects'
-    }
+      label: "Projects",
+      path: "/Projects",
+    },
   ],
   author: {
-    name: 'Ronit Mathur',
-    photo: '/RonitProfilePicture.jpg',
-    bio: 'Mind, Heart, Body, & Web Dev',
+    name: "Ronit Mathur",
+    photo: "/RonitProfilePicture.jpg",
+    bio: "Mind, Heart, Body, & Web Dev",
     contacts: {
-      email: 'ronitmathur19@gmail.com',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-      medium: '',
-    }
-  }
+      email: "ronitmathur19@gmail.com",
+      facebook: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "#",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+      medium: "",
+    },
+  },
 };
---
+++ src/Custom Pages/Projects.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Projects extends Component {
+  state = {};
+  render() {
+    return <p>Here are my current and past projects!</p>;
+  }
+}
+
+export default Projects;
