a53eafc21cba8c87f79caa3ec401c6bf7c32eba2
Ronit Mathur <38735296+Ronit-Mathur@users.noreply.github.com>
2021-04-22 15:16:16 -0700

    Control font

--- src/Custom Pages/Projects.js
+++ src/Custom Pages/Projects.js
@@ -11,7 +11,7 @@ class Projects extends Component {
       <Layout title={"Projects"} description={"My Current and Past Projects"}>
         <Sidebar />
         <Page>
-          <h1>Control with arrow keys, rotate with up arrow</h1>
+          <p>Control with arrow keys, rotate with up arrow</p>
           <TetrisFrame />
         </Page>
       </Layout>
